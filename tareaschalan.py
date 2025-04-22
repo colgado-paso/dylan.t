ac6e8e6ed7c3f266d62ac6c3bc82f4dc56e70c1a
dylan.t <dylantprdecilla026@gmail.com>
2025-04-21 23:07:18 -0400

    actualizar

---
+++ tareaschalan.py
@@ -0,0 +1,23 @@
+# edad = int(input("ingrese su edad"))
+# if edad < 12:
+#     print("usted es un niño ")
+# elif edad >= 12 and edad < 18:
+#     print("usted es un adolecente ")
+# elif edad >= 18 and edad < 65:
+#     print("usted es un adulto ")
+# else:
+#     print("usted es un anciano ")
+# num1 = int(input("numero 1:"))
+# num2 = int(input("numero 2:"))
+# operacion = input("introduce una operacion (+ - * / ): ")
+
+# match operacion:
+#     case "+":
+#         res = num1+num2
+#     case "-":
+#         res = num1-num2
+#     case "*":
+#         res = num1*num2
+#     case "/":
+#         res = num1/num2
+# print(f"el resultado de {num1} {operacion} {num2} = {res}")
