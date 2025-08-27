e5dfa032288318679e6e06b095eeb5245ec81494
dylan.t <dylantordecilla@gmail.com>
2025-08-27 09:34:02 -0400

    activar

--- codigos.py/chao.py
+++ codigos.py/chao.py
@@ -1,36 +1,12 @@
 
-num1 = int(input("ingrese el primer numero "))
-num2 = int(input("ingrese el segundo numero "))
-intentos = 3
-for i in range(num1,num2):
- 
-import random
- 
-numaleatorio = random.randint(num1,num2)
-while True:
- 
-numadiv = int(input("ingrese un numero "))
- 
-if numadiv == numaleatorio:
- 
-print("felicidades has adivinado el numero al primer intento s ")
- 
-break
- 
-elif numadiv < numaleatorio:
- 
-print(f"el numero es mayor, esta mas cerca de {num2}")
- 
-elif numadiv > numaleatorio:
- 
-print(f"el numero es mayor, esta mas cerca de {num2}")
- 
-else:
- 
-print(f"el numero es menor, esta mas cerca de {num1}")
-intentos -=1
-if intentos == 0:
- 
-print(f"perdiste el numero era {numadiv} ")
-else: 
- 
+# Como parte de un proyecto de mejora, en el año $2021$ se instaló un cable de fibra óptica que une España con EEUU. El cable tiene un largo de $6.600$ $km$ y fue colocado por un barco a una velocidad de $1,85$ $km/h$.
+
+
+
+# 1.   Determine la forma algebraica de la función que permite determinar el largo del cable instalado (en km) a partir del tiempo transcurrido (en horas). Utilice la instrucción *def* para definir la función en *Python*.
+# 2.   Defina variable dependiente e independiente, indicando unidad de medida.
+# 3.   Determine el dominio contextualizado de la función.
+# 4.   Grafique la función utilizando la biblioteca *Matplotlib* considerando el dominio contextualizado.
+# 5.   ¿Cuántos metros de cable se instalaron al transcurrir $148$ horas? ¿Y transcurridas $2.300$ horas?
+# 6.   Si se han instalado $3.480$ kilometros de cable, ¿cuántas horas llevan de trabajo?
+# 7.   ¿Cuánto tiempo transcurrió para que se completara la obra?
